3e05c6819a4db417a07eb2cef91d07bece11d637
IsmailDX <ismaildxx@gmail.com>
2024-03-02 21:55:52 +0400

    first commit

---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+import ParticlesBackground from "@/components/Particles/ParticlesBackground";
+
+export default function Home() {
+  return (
+    <main>
+      <ParticlesBackground />
+      <div className="h-[100dvh] w-full"></div>
+    </main>
+  );
+}
---
+++ components/Particles/ParticlesBackground.tsx
@@ -0,0 +1,29 @@
+"use client";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+import { useCallback } from "react";
+import options from "./options";
+import type { Container, Engine } from "tsparticles-engine";
+
+export default function ParticleBackground() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
+  return (
+    <div className="absolute -z-10 inset-0">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={options}
+      />
+    </div>
+  );
+}
